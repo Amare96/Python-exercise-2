ae08d25999734a379587f6b6443e067582e8749d
CaseyMartin23 <cm232311@gmail.com>
2020-03-09 12:12:23 +0200

    new changes

---
+++ Python/File2.py
@@ -0,0 +1,18 @@
+
+pi = 3.14
+r = 5
+A = (pi * r**2)
+print(pi * r**2)
+
+a = 4
+b = 8
+c = 4
+d = 10
+num = (a-3)**1/2
+denom = b**2 + c**2 + d**2
+x = num/denom
+print(x)
+
+version_2 = (a-3)**(1/2)/(b**2 + c**2 + d**2)
+print(version_2)
+
